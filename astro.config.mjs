0a3f5152670e13b7441fc7fe04a71429c23a5db0
f-vitorio <fabio.vitorios@gmail.com>
2026-02-28 11:08:40 -0300

    build: configurando integração react e dependências

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://valdaacupuntura.com.br",
     integrations: [
         tailwind(),
+        react(), // Adicionado para suportar componentes e ícones
         partytown({
             config: {
                 forward: ["dataLayer.push"],
